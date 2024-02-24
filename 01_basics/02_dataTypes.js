0bb09e82f9bde64451513782b12f77ddffd4c45c
RaoWAqar000 <139276285+RaoWAqar000@users.noreply.github.com>
2024-02-24 23:22:50 +0500

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all Js code in a newer version
+
+// alert(3+3) we are using NodeJs not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+    
+    let name = "Rao"
+    let age = 21
+    let isLoggedin = false
+    let state;
+
+
+    //number => 2 to power 53
+    //bigint
+    //String =>""
+    //boolean => true/false
+    //null => standalone value
+    //undefined
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof isLoggedin);
+    console.log(typeof null);
+    console.log(typeof undefined);
