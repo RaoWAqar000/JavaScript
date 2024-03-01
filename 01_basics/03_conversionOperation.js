03d77c9614a99cf3fe85c4cfd26b070a9082a5c8
RaoWAqar000 <139276285+RaoWAqar000@users.noreply.github.com>
2024-03-01 22:53:01 +0500

    Add files via upload
    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,80 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not an number)
+// true => 1
+// false=> 0
+
+// let a = 1
+// let a = 0
+// let a = ""
+let a = "Rao"
+
+let b = Boolean(a)
+// console.log(b);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Rao" => true
+
+let c = 33
+
+let d = String(c)
+// console.log(d);
+// console.log(typeof d);
+
+//******************* Operations **********************
+
+let value = 5
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Rao"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(((3+4)*5)%6);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+3
+
+let j = 100
+// j++;
+++j;
+// console.log(j);
+
+let x = 3;
+const y = ++x; 
+console.log(y);
+
+let s = 3;
+const o = s++; 
+console.log(o);
