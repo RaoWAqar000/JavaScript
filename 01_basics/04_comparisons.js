aabc79739a788c649f13f53d613b23362664a693
RaoWAqar000 <139276285+RaoWAqar000@users.noreply.github.com>
2024-03-01 22:55:05 +0500

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("2" > false);
+// console.log(1 > true);
+// console.log(true < 0);
+
+//true == 1 
+//false == 0
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+ 
